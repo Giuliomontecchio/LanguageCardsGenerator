0e7b6e661989fd54975eb8c5366a3ccfd9c3f41a
Biglio <giulio.montecchio.gm@gmail.com>
2025-08-22 18:31:28 +0200

    format code with black

--- main.py
+++ main.py
@@ -8,38 +8,44 @@ from bs4 import BeautifulSoup
 from st_clickable_images import clickable_images
 import os
 
+
 # Parser function
 def create_list_of_cards(src_text: str) -> list[dict]:
     """
-Creates a list of dictionaries corresponding to fields from a given text.
+    Creates a list of dictionaries corresponding to fields from a given text.
 
-Args:
-    src_text (str): The text to parse, with each line representing a record
-        and each field separated by a '|' character.
+    Args:
+        src_text (str): The text to parse, with each line representing a record
+            and each field separated by a '|' character.
 
-Returns:
-    list[dict]: A list of dictionaries, where each dictionary represents a card
-        with fields 'baseT', 'baseS', 'fullT', 's1T', 's1S', 's2T', and 's2S'.
-"""
+    Returns:
+        list[dict]: A list of dictionaries, where each dictionary represents a card
+            with fields 'baseT', 'baseS', 'fullT', 's1T', 's1S', 's2T', and 's2S'.
+    """
     list_of_cards = []
     header = ["baseT", "baseS", "fullT", "s1T", "s1S", "s2T", "s2S"]
     lines = src_text.strip().splitlines()
     for line in lines:
         if line.strip():  # Check if the line is not empty
             parts = line.split("|")
-            record = {header[i]: parts[i].strip() if i < len(parts) else "" for i in range(len(header))}
+            record = {
+                header[i]: parts[i].strip() if i < len(parts) else ""
+                for i in range(len(header))
+            }
             list_of_cards.append(record)
     return list_of_cards
 
+
 def color_gender(field, selected_language):
     if selected_language == "de":
-        if field.startswith("die "): 
+        if field.startswith("die "):
             field = f'<span style="color: rgb(255, 88, 111);">{field}</span>'
         elif field.startswith("das "):
             field = f'<span style="color: rgb(88, 255, 101);">{field}</span>'
         elif field.startswith("der "):
             field = f'<span style="color: rgb(88, 141, 255);">{field}</span>'
     return field
+
 
 def create_note(fields, selected_language):
 
@@ -53,52 +59,57 @@ def create_note(fields, selected_language):
     }"""
 
     model_card_generator = genanki.Model(
-    1284830180,
-    'Language (and reversed card) card generator',
-    fields=[
-        {'name': 'baseS'},
-        {'name': 'baseT'},
-        {'name': 'AbaseT'},
-        {'name': 'fullT'},
-        {'name': 'AfullT'},
-        {'name': 's1T'},
-        {'name': 'As1T'},
-        {'name': 's2T'}, 
-        {'name': 'As2T'},
-        {'name': 'image1'},
-        {'name': 'image2'},
+        1284830180,
+        "Language (and reversed card) card generator",
+        fields=[
+            {"name": "baseS"},
+            {"name": "baseT"},
+            {"name": "AbaseT"},
+            {"name": "fullT"},
+            {"name": "AfullT"},
+            {"name": "s1T"},
+            {"name": "As1T"},
+            {"name": "s2T"},
+            {"name": "As2T"},
+            {"name": "image1"},
+            {"name": "image2"},
         ],
-    templates=[
-    {'name': 'Card 1',
-    'qfmt': '{{baseS}}<br>{{image1}} {{image2}}',
-    'afmt': '{{baseS}}<br>{{image1}} {{image2}} <br> {{fullT}} {{AfullT}}'
-    '<hr id="answer">{{s1T}} {{As1T}}<br> {{s2T}} {{As2T}}',
-    },
-    {'name': 'Card 2',
-    'qfmt': '{{baseT}} {{AbaseT}}',
-    'afmt': '{{fullT}} {{AfullT}}<br>{{baseS}}'
-    '<hr id="answer">{{image1}} {{image2}}<br> {{s1T}}{{As1T}}<br> {{s2T}} {{As2T}}',
-    }
-    ],
-    css = css)
+        templates=[
+            {
+                "name": "Card 1",
+                "qfmt": "{{baseS}}<br>{{image1}} {{image2}}",
+                "afmt": "{{baseS}}<br>{{image1}} {{image2}} <br> {{fullT}} {{AfullT}}"
+                '<hr id="answer">{{s1T}} {{As1T}}<br> {{s2T}} {{As2T}}',
+            },
+            {
+                "name": "Card 2",
+                "qfmt": "{{baseT}} {{AbaseT}}",
+                "afmt": "{{fullT}} {{AfullT}}<br>{{baseS}}"
+                '<hr id="answer">{{image1}} {{image2}}<br> {{s1T}}{{As1T}}<br> {{s2T}} {{As2T}}',
+            },
+        ],
+        css=css,
+    )
 
-    fields_note = [fields['baseS']]
+    fields_note = [fields["baseS"]]
     # generate audio
-    for i, key in enumerate(['baseT','fullT', 's1T', 's2T']):
+    for i, key in enumerate(["baseT", "fullT", "s1T", "s2T"]):
         if fields[key] == "":
-            fields_note.extend([fields[key], ''])
+            fields_note.extend([fields[key], ""])
             continue
         sound = gTTS(text=fields[key], lang=selected_language, slow=False)
         sound.save(f"sound{st.session_state.index}_{i}.mp3")
         st.session_state.all_media.append(f"sound{st.session_state.index}_{i}.mp3")
-        if key == 'fullT':
-            fields['fullT'] = color_gender(fields['fullT'], selected_language)
-        fields_note.extend([fields[key], f'[sound:sound{st.session_state.index}_{i}.mp3]'])
+        if key == "fullT":
+            fields["fullT"] = color_gender(fields["fullT"], selected_language)
+        fields_note.extend(
+            [fields[key], f"[sound:sound{st.session_state.index}_{i}.mp3]"]
+        )
 
     # Add images dynamically
     images = [f'<img src="{img_name}">' for img_name in st.session_state.image_filename]
     while len(images) < 2:  # Ensure 2 image placeholders
-        images.append('')
+        images.append("")
 
     fields_note.extend(images)  # Add images to the fields
 
@@ -114,6 +125,7 @@ def create_note(fields, selected_language):
     st.session_state.image_filename = []
     st.session_state.image_urls_to_add = []
 
+
 def create_deck():
     # Create the Anki package
     package = genanki.Package(st.session_state.deck)
@@ -121,16 +133,19 @@ def create_deck():
 
     # Write package to in-memory buffer instead of file
     output = BytesIO()
-    package.write_to_file(output)  # This works because genanki supports file-like objects
+    package.write_to_file(
+        output
+    )  # This works because genanki supports file-like objects
 
     # Move the cursor to the beginning of the buffer
     output.seek(0)
 
     # Save in session state for later use (e.g., download)
     st.session_state.apkg_data = output.read()
-    st.session_state.file_name = f'{st.session_state.deck.name}.apkg'
+    st.session_state.file_name = f"{st.session_state.deck.name}.apkg"
 
-def get_image_urls(keyword:str, subdomain:str)->list[str]:
+
+def get_image_urls(keyword: str, subdomain: str) -> list[str]:
     # Starting from a keyword, creates a list of URLs which direct to images.
 
     # Create a Google Images URL for the search term
@@ -138,15 +153,16 @@ def get_image_urls(keyword:str, subdomain:str)->list[str]:
     # Send an HTTP GET request to the URL
     response = requests.get(search_url, timeout=10)
     # Parse the HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
     # Find image links in the page source
     list_of_tags = soup.find_all("img")
     list_of_urls = []
     for tag in list_of_tags:
         list_of_urls.append(tag.get("src"))
-    # On google image, first one is google logo 
+    # On google image, first one is google logo
     list_of_urls.pop(0)
     return list_of_urls
+
 
 def load_images(urls):
     """Download and return images from URLs."""
@@ -158,8 +174,10 @@ def load_images(urls):
             images.append(img)
     return images
 
+
 def reset_app():
     st.session_state.reset = True
+
 
 def main():
     if st.session_state.get("reset"):
@@ -172,7 +190,7 @@ def main():
                     os.remove(path)
                 except Exception as e:
                     st.warning(f"Could not delete {path}: {e}")
-        
+
         # Clean session states
         for key in list(st.session_state.keys()):
             del st.session_state[key]
@@ -181,19 +199,19 @@ def main():
 
     st.set_page_config(page_title="Anki Card Generator")
     # Initialize deck and all_media in session_state (if not already initialized)
-    if 'cards' not in st.session_state:
+    if "cards" not in st.session_state:
         st.session_state.cards = []
         st.session_state.current_card = 0
         st.session_state.submitted = False  # Track submission state
     if "all_media" not in st.session_state:
         st.session_state.all_media = []  # Store media lis
-    if 'index' not in st.session_state: # index to go up for each added card?
+    if "index" not in st.session_state:  # index to go up for each added card?
         st.session_state.index = 0
-    if 'image_urls_to_add' not in st.session_state:
+    if "image_urls_to_add" not in st.session_state:
         st.session_state.image_urls_to_add = []
-    if 'image_filename' not in st.session_state:
+    if "image_filename" not in st.session_state:
         st.session_state.image_filename = []
-    if 'image_viewer_urls' not in st.session_state:
+    if "image_viewer_urls" not in st.session_state:
         st.session_state.image_viewer_urls = []
 
     if not st.session_state.submitted:
@@ -201,27 +219,28 @@ def main():
         language_options = ["de", "es", "fr"]
 
         # Create the selectbox
-        selected_language = st.selectbox(
-            "Choose a language",
-            language_options,
-            index = 0
-        )
+        selected_language = st.selectbox("Choose a language", language_options, index=0)
 
         st.session_state.selected_language = selected_language
 
-        user_input = st.text_area("Paste your text here (use '|' as field delimiter, one line per card):")
+        user_input = st.text_area(
+            "Paste your text here (use '|' as field delimiter, one line per card):"
+        )
 
         if st.button("Submit"):
             if user_input:
                 st.session_state.cards = create_list_of_cards(user_input)
                 st.session_state.current_card = 0
                 st.session_state.submitted = True  # Hide inputs after submission
-                if 'deck' not in st.session_state and 'selected_language' in st.session_state:
+                if (
+                    "deck" not in st.session_state
+                    and "selected_language" in st.session_state
+                ):
                     deck_info = {
                         "de": (87654321, "Deutsch"),
                         "es": (87654322, "Espanol"),
-                        "fr": (87654323, "Francais")
-                        }
+                        "fr": (87654323, "Francais"),
+                    }
                     lang = st.session_state.selected_language
                     deck_id, deck_name = deck_info.get(lang, (87654321, "Deutsch"))
                     st.session_state.deck = genanki.Deck(deck_id, deck_name)
@@ -245,14 +264,18 @@ def main():
                     images_to_save = load_images(st.session_state.image_urls_to_add)
                     st.session_state.image_filename = []
                     for i, image in enumerate(images_to_save):
-                        st.session_state.image_filename.append(f"image{st.session_state.index}_{i}.png")
+                        st.session_state.image_filename.append(
+                            f"image{st.session_state.index}_{i}.png"
+                        )
                         image.save(f"image{st.session_state.index}_{i}.png")
                     st.success("Images saved successfully!")
                     st.session_state.image_viewer_urls = []
                     del st.session_state["image_clicked"]
                     st.rerun()
-                
-            if "clicked" not in st.session_state:  # Ensure `clicked` is only computed once
+
+            if (
+                "clicked" not in st.session_state
+            ):  # Ensure `clicked` is only computed once
                 st.session_state.image_clicked = clickable_images(
                     st.session_state.image_viewer_urls,
                     div_style={
@@ -260,29 +283,33 @@ def main():
                         "justify-content": "center",
                         "flex-wrap": "wrap",
                         "gap": "2px",  # Space between images
-                        "padding": "1px"
+                        "padding": "1px",
                     },
                     img_style={
-                        "margin": "5px", 
-                        "max-width": "none", 
-                        "max-height": "none", 
-                        "width": "auto", 
-                        "height": "auto", 
-                        "object-fit": "contain"
+                        "margin": "5px",
+                        "max-width": "none",
+                        "max-height": "none",
+                        "width": "auto",
+                        "height": "auto",
+                        "object-fit": "contain",
                     },
-                    key="image_viewer"
+                    key="image_viewer",
                 )
 
-            if st.session_state.image_clicked>-1:
+            if st.session_state.image_clicked > -1:
                 st.subheader("Selected images")
-                st.session_state.image_urls_to_add.append(st.session_state.image_viewer_urls[st.session_state.image_clicked])
-                st.session_state.image_urls_to_add = st.session_state.image_urls_to_add[-2:]
+                st.session_state.image_urls_to_add.append(
+                    st.session_state.image_viewer_urls[st.session_state.image_clicked]
+                )
+                st.session_state.image_urls_to_add = st.session_state.image_urls_to_add[
+                    -2:
+                ]
                 st.image(st.session_state.image_urls_to_add)
 
         if st.session_state.cards and not st.session_state.image_viewer_urls:
             current_card = st.session_state.current_card
             fields = st.session_state.cards[current_card]
-            
+
             col1, col2, col3, col4, col5 = st.columns(5)
 
             if col1.button("Previous") and current_card > 0:
@@ -290,15 +317,17 @@ def main():
 
             if col3.button("Next") and current_card < len(st.session_state.cards) - 1:
                 st.session_state.current_card += 1
-            
+
             current_card = st.session_state.current_card
             fields = st.session_state.cards[current_card]
 
             with col2:
                 st.write(f"Card {current_card + 1}/{len(st.session_state.cards)}")
-            
+
             if col4.button("Image"):
-                st.session_state.image_viewer_urls = get_image_urls(fields['baseT'], st.session_state.selected_language)
+                st.session_state.image_viewer_urls = get_image_urls(
+                    fields["baseT"], st.session_state.selected_language
+                )
                 st.rerun()
 
             if col5.button("Add Card"):
@@ -313,17 +342,19 @@ def main():
 
             if st.button("Add Deck"):
                 create_deck()
-                
-            if 'apkg_data' in st.session_state:
+
+            if "apkg_data" in st.session_state:
                 st.download_button(
                     label="Download Anki Deck",
                     data=st.session_state.apkg_data,
                     file_name=st.session_state.file_name,
                     mime="application/octet-stream",
-                    on_click="ignore"
+                    on_click="ignore",
                 )
                 if st.button("Reset"):
                     reset_app()
                     st.rerun()
+
+
 if __name__ == "__main__":
     main()
